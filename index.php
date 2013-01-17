82ce1c04b63196a94e05c9f152484f2bb59759f7
William Estrada <william@seabourneconsulting.com>
2013-01-16 17:33:46 -0800

    Output for capistrano demo

--- index.php
+++ index.php
@@ -16,6 +16,8 @@
  */
 define('DRUPAL_ROOT', getcwd());
 
+echo 'Demo Capistrano';
+
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
